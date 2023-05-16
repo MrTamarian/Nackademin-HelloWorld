ca91d14583ff0a9393d471fefd794ba5df99721b
Gogi <gogi.akin@gmail.com>
2023-05-16 14:49:22 +0200

    Lotr added

---
+++ server/testing.py
@@ -0,0 +1,7 @@
+listof_names = ["aragorn", "frodo", "gimli", "gandalf"]
+
+for name in listof_names:
+    print(f"The fire cannot avail you {name.title()}!")
+listof_names.append("Sinan")
+
+print(listof_names)
